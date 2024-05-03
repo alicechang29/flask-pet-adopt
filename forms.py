24b699bb382a022a223f969ee0daadd025bbe3db
Zach Bunting <zvrbunting@gmail.com>
2024-05-03 15:30:09 -0700

    Finished validation.

--- forms.py
+++ forms.py
@@ -2,19 +2,35 @@
 
 from flask_wtf import FlaskForm
 from wtforms import StringField, FloatField, SelectField, TextAreaField
-from wtforms.validators import InputRequired, Optional, Email
+from wtforms.validators import InputRequired, Optional, Email, AnyOf, URL
 
 
 class AddPetForm(FlaskForm):
     """Form for adding  pets."""
 
     name = StringField("Pet Name")
-    species = StringField("Species")
-    photo_url = StringField("Photo URL")
+    species = StringField(
+        "Species",
+        validators=[AnyOf(values=['cat', 'dog', 'porcupine'])]
+    )
+    photo_url = StringField(
+        "Photo URL",
+        validators=[
+            URL(require_tld=False),
+            Optional()]
+    )
     age = SelectField("Age",
-                      choices=[('baby', 'Baby'),
-                               ('young', 'Young'),
-                               ('adult', 'Adult'),
-                               ('senior', 'Senior')
-                               ])
+                      choices=[
+                          ('baby', 'Baby'),
+                          ('young', 'Young'),
+                          ('adult', 'Adult'),
+                          ('senior', 'Senior')
+                      ],
+                      validators=[AnyOf(values=[
+                          'baby',
+                          'young',
+                          'adult',
+                          'senior'
+                      ])]
+                      )
     notes = TextAreaField("Notes")
